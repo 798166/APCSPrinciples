fc19e1ad04589f04fc2b347d9261031377ad12d1
798166 <54591243+798166@users.noreply.github.com>
2019-09-06 08:50:11 -0700

    atari

---
+++ Labs/Lab906 Rotation/Ship.js
@@ -0,0 +1,88 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+class Ball{
+  constructor(x, y, dx, dy, id){
+    this.loc = createVector(x,y);
+    this.vel = createVector(dx,dy);
+    this.acc = createVector(0,0)
+    this.clr = color(random(255), random(255), random(255));
+    this.w = 15;
+    this.id = id;
+    if(this.id < 0) {this.w = 50;}
+    else{
+      this.w = 15
+    }
+  }
+run(){
+  this.checkEdges();
+  this.update();
+  this.render();
+  }
+
+checkEdges(){
+  if(this.id>=0){
+  if(this.loc.x < 0){
+    this.vel.x = -this.vel.x;
+  }
+  if(this.loc.x > width){
+    this.vel.x = -this.vel.x;
+  }
+  if(this.loc.y < 0){
+    this.vel.y = -this.vel.y;
+  }
+  if(this.loc.y > height){
+    this.vel.y = -this.vel.y;
+  }
+}
+  if(this.id<0){
+    if(this.loc.x>800){
+      //this.loc.x=0 //mainBall warp
+      this.vel.x = -this.vel.x; //mainBall bounce off wall
+    }
+    if(this.loc.x<0){
+      //this.loc.x=800 //mainBall warp
+      this.vel.x = -this.vel.x; //mainBall bounce off wall
+    }
+    if(this.loc.y>800){
+      //this.loc.y=0 //mainBall warp
+      this.vel.y = -this.vel.y; //mainBall bounce off wall
+    }
+    if(this.loc.y<0){
+      //this.loc.y=800 //mainBall warp
+      this.vel.y = -this.vel.y; //mainBall bounce off wall
+    }
+  }
+}
+  update(){
+    var distTomainBall;
+    if(this.id>=0){
+      distTomainBall = this.loc.dist(mainBall.loc);
+      //attraction
+      if (distTomainBall<200){
+        this.acc = p5.Vector.sub(mainBall.loc, this.loc);
+        this.acc.normalize();
+        this.acc.mult(0.1);
+      }
+      //repulsion
+      if(distTomainBall<100){
+        this.acc = p5.Vector.sub(this.loc, mainBall.loc);
+        this.acc.normalize();
+        this.acc.mult(0.5);
+      }
+    }
+    this.vel.limit(5);
+    this.loc.add(this.vel);
+    this.vel.add(this.acc);
+  }
+  render(){
+    this.angle = this.angle + .1;
+    push();
+    translate(this.loc.x, this.loc.y);
+    rotate(this.angle);
+    triangle(-5,8,5,8,0,-8)
+    //this.clr = color(random(255), random(255), random(255));
+    fill(this.clr);
+  }
+}
