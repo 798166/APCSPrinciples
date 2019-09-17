9520299f89bfdbb3166f2ec59c6c3d11fd6e11aa
William Kreidler <willkreidler@Williams-MacBook-Pro.local>
2019-09-16 22:48:04 -0700

    balls appeared

--- Labs/Lab912 Paddle/Ball.js
+++ Labs/Lab912 Paddle/Ball.js
@@ -3,45 +3,40 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 class Ball{
-  constructor(x, y, dx, dy){
-    this.loc = createVector(x,y);
-    this.vel = createVector(dx,dy);
-    this.acc = createVector(0,1)
+  constructor(x, y, dx, dy) {
     this.clr = color(random(255), random(255), random(255));
-  }
-
-  run(){
-    this.checkEdges();
-    this.update();
-    this.render();
-    this.isColliding();
-  }
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0, .1);
+} //end of constructor
+run(){
+  this.render();
+  this.checkEdges();
+  this.update();
+  this.isColliding();
+} //end of run
+render(){
+  fill(this.clr);
+  ellipse(this.loc.x, this.loc.y, 20, 20);
+} //end of render
 
   checkEdges(){
-    if(this.loc.x<0 || this.loc.x>width)
-      this.vel.x = -this.vel.x
-    if(this.loc.y<0 || this.loc.y>height)
-      this.vel.y = -this.vel.y
-  }
+    if(this.loc.x < 0 || this.loc.x > width)this.vel.x = -this.vel.x;
+    if(this.loc.y < 0 || this.loc.y > height) this.vel.y = -this.vel.y;
+  } //end of checkEdges
 
-  update(){
-    this.vel.limit(5);
-    this.loc.add(this.vel);
-    this.vel.add(this.acc);
-  }
+update(){
+  //this.clr = color(random(255), random(255), random(255));
+  this.loc.add(this.vel);
+  this.vel.add(this.acc);
+} //end of update
 
-  render(){
-    //this.clr = color(random(255), random(255), random(255));
-    fill(this.clr);
-    ellipse(this.loc.x, this.loc.y, this.w, this.w);
-  }
-
-  isColliding(){
-    if(this.loc.x > paddle.loc.x &&
-      this.loc.x < paddle.loc.x + paddle.w &&
-      this.loc.y > paddle.loc.y &&
-      this.loc.y < paddle.loc.y + paddle.h){
-        this.vel.y = -this.vel.y;
-    }
-  }
+isColliding(){
+  if(this.loc.x > paddle.loc.x &&
+  this.loc.x < paddle.loc.x +paddle.w &&
+  this.loc.y > paddle.loc.y &&
+  this.loc.y < paddle.loc.y + paddle.h){
+    this.vel.y = -this.vel.y;
 }
+}
+};
--- Labs/Lab912 Paddle/sketch.js
+++ Labs/Lab912 Paddle/sketch.js
@@ -2,34 +2,29 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var balls = [];
+var balls = []
 var paddle;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
-  loadObjects(5);//  b1 = new Ball(random(width), random(height), random(-9,9), random(-9,9));
-//  b2 = new Ball(random(width), random(height), random(-9,9), random(-9,9));
+  background(20, 20, 20);
+  loadObjects(3);
 }
 
-//  The draw function is called @ 30 fps
 function draw() {
-  background(20, 20 ,20, 20);
-  runObjects();// b1.run();
-// b2.run();
+  background(205,24,223, 60);
+  runObjects();
+  fill(2, 2, 2)
 }
 
 function loadObjects(n){
-  for(var i = 0; i < n; i++){
-    balls[i] = new Ball(random(width), random(height), random(-5, 5),random(-10, 10));
+  for(var i =0; i < n; i++){
+    balls[i] = new Ball(random(800), random(300), random(0, 5), random(0,5));
   }
   paddle = new Paddle(250, 700, 200, 25);
 }
 
 function runObjects(){
   paddle.run();
-  for(var i = 0; i < balls.length; i++){
-    balls[i].run();
-  }
+  for(var i = 0; i < balls.length; i++) balls[i].run();
 }
