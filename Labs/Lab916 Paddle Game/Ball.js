6491c6365fc40de07212cfdfa90ef44d4192198f
798166 <54591243+798166@users.noreply.github.com>
2019-09-17 08:50:44 -0700

    works

---
+++ Labs/Lab916 Paddle Game/Ball.js
@@ -0,0 +1,42 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+class Ball{
+  constructor(x, y, dx, dy) {
+    this.clr = color(random(255), random(255), random(255));
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0, .1);
+} //end of constructor
+run(){
+  this.render();
+  this.checkEdges();
+  this.update();
+  this.isColliding();
+} //end of run
+render(){
+  fill(this.clr);
+  ellipse(this.loc.x, this.loc.y, 20, 20);
+} //end of render
+
+  checkEdges(){
+    if(this.loc.x < 0 || this.loc.x > width)this.vel.x = -this.vel.x;
+    if(this.loc.y < 0 || this.loc.y > height) this.vel.y = -this.vel.y;
+  } //end of checkEdges
+
+update(){
+  //this.clr = color(random(255), random(255), random(255));
+  this.loc.add(this.vel);
+  this.vel.add(this.acc);
+} //end of update
+
+isColliding(){
+  if(this.loc.x > paddle.loc.x &&
+  this.loc.x < paddle.loc.x +paddle.w &&
+  this.loc.y > paddle.loc.y &&
+  this.loc.y < paddle.loc.y + paddle.h){
+    this.vel.y = -this.vel.y;
+}
+}
+};
---
+++ Labs/Lab916 Paddle Game/Paddle.js
@@ -0,0 +1,27 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+class Paddle{
+  constructor(x, y, w, h){
+    this.loc = createVector(x,y);
+    this.clr = color(random(255), random(255), random(255));
+    this.w = 300;
+    this.h = 25;
+    }
+  run(){
+    this.update();
+    this.render();
+  }
+
+  update(){
+    var mouseLoc = createVector(mouseX, 700)
+    this.loc = p5.Vector.lerp(this.loc, mouseLoc, 0.09);
+  }
+
+  render(){
+    //this.clr = color(random(255), random(255), random(255));
+    fill(this.clr);
+    rect(this.loc.x, this.loc.y, this.w, this.h);
+  }
+}
