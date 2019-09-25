beac64f60fd801a011bc376c40a133bf95fc5dc5
Will Krei <wkrei@WS-ACADBUS.EDU>
2019-09-25 08:27:09 -0700

    925

--- Labs/Lab916 Paddle Game/sketch.js
+++ Labs/Lab916 Paddle Game/sketch.js
@@ -1,5 +1,5 @@
 //  Your Name
-// 	Date or version number
+// 	925  Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
 var balls = []
