46330822b8fff6d2ee37ef42b5831c7f00db774e
798166 <54591243+798166@users.noreply.github.com>
2019-10-21 08:49:33 -0700

    sort

---
+++ Labs/Lab1021 InsertionSort/sketch.js
@@ -0,0 +1,23 @@
+//  Will Kreidler
+// 	1009
+//  This is a comment
+//  The setup function function is called once when your program begins
+function setup() {
+  var list = [4, 2, 1, 5, 7, 9];
+
+  function swap(){
+      if(list[a]>list[b]){
+        //swapping code
+        var temp = list[a];
+        list[a] = list[b];
+        list[b] = temp;
+      }
+
+      //swapping code
+    }
+  console.log(list);
+}
+
+function draw(){
+
+}
