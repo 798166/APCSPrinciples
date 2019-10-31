a0ee858a9401669882399139e86d503533144e23
798166 <54591243+798166@users.noreply.github.com>
2019-10-31 08:49:35 -0700

    snake

---
+++ Labs/Lab1031 snakeGame/sketch.js
@@ -0,0 +1,17 @@
+//  Will Kreidler
+// 	1031
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+ellipse(200, 320, 300, 269);
+fill(200, 148, 0);
+}
