508af2ec6d5b53b1c421ab79c49c5d30e202ba98
798166 <54591243+798166@users.noreply.github.com>
2019-11-01 08:48:23 -0700

    snake

---
+++ Labs/Lab1031 snakeGame/food.js
@@ -0,0 +1,11 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+class Food{
+  constructor(x, y, w, h, clr){
+    this.loc = createVector(x,y)
+    this.w = w;
+    this.h = h;
+  }
+}//end of snake class
--- Labs/Lab1031 snakeGame/snake.js
+++ Labs/Lab1031 snakeGame/snake.js
@@ -3,9 +3,39 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 class Snake{
-  constructor(x, y, w, h, clr){
+  constructor(x, y, dx, dy, w, id){
     this.loc = createVector(x,y)
-    this.w = w;
-    this.h = h;
-  })
+    this.loc = createVector(dx,dy)
+    this.w = 25;
+    this.id=id;
+  }
+
+  run(){
+    this.render();
+    this.checkEdges();
+    this.update();
+  }
+
+  render(){
+    background(20,20,20);
+    fill(this.clr);
+    rect(this.loc.x, this.loc.y,this.w,this.w);
+  }
+
+  checkEdges(){
+
+  }
+
+  update(){
+    this.vel=0;
+    if(keyIsPressed&&
+      keyCode=== RIGHT_ARROW){
+        this.vel=3;
+      }
+      if(keyIsPressed&&
+        keyCode=== RIGHT_ARROW){
+        this.vel=3;
+        }
+    this.loc.add(this.vel);
+  }
 }//end of snake class
