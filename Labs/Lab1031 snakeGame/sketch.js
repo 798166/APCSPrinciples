1986832690c40fbdabb19620d3a06d7c9a32fa49
798166 <54591243+798166@users.noreply.github.com>
2019-11-22 08:48:51 -0800

    ho

--- Labs/Lab1031 snakeGame/sketch.js
+++ Labs/Lab1031 snakeGame/sketch.js
@@ -28,6 +28,9 @@ function draw() {
   if(gameState===3){
     finalScreen();
   }
+  if(gameState===4){
+    instructionScreen();
+  }
 }
 
 function startScreen(){
@@ -35,6 +38,11 @@ function startScreen(){
   textSize(75);
   fill(37, 48, 89);
   text('Snake Game',200,200);
+  textSize(25);
+  fill(0,0,0)
+  text('Instructions: Use the arrow keys to move the snake.',180,350);
+  text('Try and eat as much food as possible without touching',180, 375);
+  text('the edge or your body. Good Luck!!',180, 400);
   textSize(25);
   fill(0,255,0)
   text('Start',375,550);
@@ -67,10 +75,11 @@ function finalScreen(){
   background(20,20,20);
   textSize(25);
   fill(255,0,0);
-  text('You lose! You got bad score of '+ numberFood, 225,400);
-  //textSize(25);
-  //fill(255,0,0);
+  text('You DIED! You got low score of '+ numberFood, 225,400);
   text('Want to play again? Press reload on the tab!', 225,525);
+  textSize(100);
+  fill(255,0,0);
+  text('ERROR',225,250);
   //if(keyIsPressed&&
     //keyCode===SHIFT){
       //clear();
